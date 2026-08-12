0bbfddb95f4dacf7ed4be890d8cb3eaa417014fe
LiYan <695088328@qq.com>
2026-08-12 11:08:53 +0800

    1.0 aab

--- Assets/scripts/Ad/AdManager.cs
+++ Assets/scripts/Ad/AdManager.cs
@@ -32,7 +32,7 @@ public class AdManager : MonoBehaviour
         };
 
         string decryptedSdkKey = EncryptSDKKey.DecryptWithRandomSalt(SDK_key);
-        Debug.Log("解密结果（还原原值）：" + decryptedSdkKey);
+        //Debug.Log("解密结果（还原原值）：" + decryptedSdkKey);
         MaxSdk.SetSdkKey(decryptedSdkKey);
         MaxSdk.InitializeSdk();
     }
